3072bca4759ad97c5ccb7e374f4357cbc8a79065
Leonardo <ljcl79@gmail.com>
2022-05-23 20:54:24 -0300

    Aula1

---
+++ ByteBank/CuentaBancaria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class CuentaBancaria
+    {
+        public string nombreCliente;
+        public string numeroCuenta;
+        public string numeroAgencia;
+        public double saldo;
+        public double tasaInteres;
+        public double limiteSobregiro;
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,33 @@
+﻿using ByteBank;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Aplicación de cuentas bancarias!");
+
+CuentaBancaria cuentaLeonardo = new CuentaBancaria();
+
+cuentaLeonardo.nombreCliente = "Leonardo José";
+cuentaLeonardo.numeroCuenta = "0134-0337-61-5897564";
+cuentaLeonardo.numeroAgencia = "0134";
+cuentaLeonardo.saldo = 1000.00;
+cuentaLeonardo.tasaInteres = 7.5;
+cuentaLeonardo.limiteSobregiro = 500.00;
+
+CuentaBancaria cuentaMaria = new CuentaBancaria();
+
+cuentaMaria.nombreCliente = "María Rivas";
+cuentaMaria.numeroCuenta = "0168-0337-68-5897564";
+cuentaMaria.numeroAgencia = "0168";
+cuentaMaria.saldo = 1000.00;
+cuentaMaria.tasaInteres = 7.5;
+cuentaMaria.limiteSobregiro = 500.00;
+
+Console.WriteLine("Nombre de cliente: " + cuentaLeonardo.nombreCliente);
+Console.WriteLine("Número de cuenta: " + cuentaLeonardo.numeroCuenta);
+Console.WriteLine("Número de agencia: " + cuentaLeonardo.numeroAgencia);
+Console.WriteLine("Saldo: " + cuentaLeonardo.saldo);
+Console.WriteLine("Tasa de interés: " + cuentaLeonardo.tasaInteres);
+Console.WriteLine("Límite de sobregiro: " + cuentaLeonardo.limiteSobregiro);
+
+Console.WriteLine("Nombre de cliente: " + cuentaMaria.nombreCliente);
+
+Console.ReadLine();
